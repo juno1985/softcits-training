75ade1693ab55d6d99eeb42075da704045f6cacf
Jun <wangzhen_tju@126.com>
2017-12-23 21:15:55 +0800

    增加filefilter的几种写法

--- 初级/第10课/示例代码/1.2/FileFilter.java
+++ 初级/第10课/示例代码/1.2/FileFilter.java
@@ -1,13 +1,32 @@
+package FileListFilterDemo;
+/*1.2*/
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 
 public class FileFilter {
 	public static void main(String[] args) throws IOException{
-		File file = new File(".");
 		
+		//指定当前路径，即workspace项目路径
+		File file = new File(".");
 
-		String[] fileList = file.list((dir, name) -> dir.canRead() || name.endsWith(".txt"));
+		//1，正常写法
+		String[] fileList = file.list(new FileFilterImpl());
+		//2，匿名类写法
+	/*	String[] fileList = file.list(new FilenameFilter(){
+
+			@Override
+			public boolean accept(File dir, String name) {
+				
+				return name.endsWith(".txt");
+			}
+			
+		});*/
+		//3,JAVA8以后新增加的Lamba表达式
+	/*	String[] fileList = file.list(
+				(dir, name) ->  name.endsWith(".txt")
+				);*/
+		
 		for(String fileName : fileList)
 			System.out.println(fileName);
 		
